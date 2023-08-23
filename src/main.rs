3c5f7fd16993aef21c4dcb68809951bfca948a31
Verademora <drhax123@gmail.com>
2023-08-23 03:11:58 -0500

    prints file header debug

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 #![allow(dead_code)]
+use byteorder::{LittleEndian, ReadBytesExt};
 use std::{
     fs::File,
-    io::{self, BufReader, Read, Error},
+    io::{self, BufReader, Error, Read},
 };
 
 pub mod lib {
+
+    #[derive(Debug, Default)]
     pub struct SystemTime {
-        pub year: u32,
-        pub month: u32,
-        pub day_of_week: u32,
-        pub day: u32,
-        pub hour: u32,
-        pub minute: u32,
-        pub second: u32,
-        pub millisecond: u32,
+        pub year: u16,
+        pub month: u16,
+        pub day_of_week: u16,
+        pub day: u16,
+        pub hour: u16,
+        pub minute: u16,
+        pub second: u16,
+        pub millisecond: u16,
     }
 
-
+    #[derive(Debug, Default)]
     pub struct FileHeader {
         pub file_id: String,
         pub major_version: u8,
@@ -24,49 +27,63 @@ pub mod lib {
         pub system_time: SystemTime,
     }
 
+    impl SystemTime {
+        pub fn new(fields: &[u16]) -> Self {
+            if fields.len() != 8 {
+                panic!("Provided vector is not the right size");
+            }
+
+            Self {
+                year: fields[0],
+                month: fields[1],
+                day_of_week: fields[2],
+                day: fields[3],
+                hour: fields[4],
+                minute: fields[5],
+                second: fields[6],
+                millisecond: fields[7],
+            }
+        }
+    }
 }
 
 pub const HEADER_SIZE: usize = 30;
 pub const ID_SIZE: usize = 12;
 
 fn get_header(reader: &mut dyn Read) -> lib::FileHeader {
-
     let mut header_buffer = [0; HEADER_SIZE];
     let _ = reader.read(&mut header_buffer);
 
-    let file_id = String::new();
+    let mut file_id = String::new();
     for b in &header_buffer[..12] {
-        dbg!(b);
+        file_id.push(*b as char);
     }
-    
 
-    let major_version = 0;
-    let minor_version = 0;
-    let system_time = lib::SystemTime {
-        year: 0,
-        month: 0,
-        day_of_week: 0,
-        day: 0,
-        hour: 0,
-        minute: 0,
-        second: 0,
-        millisecond: 0,
-    };
+    let [major_version, minor_version] = &header_buffer[12..14] else { todo!() };
 
-    let k = lib::FileHeader {
+    let mut system_time_v: Vec<u16> = Vec::new();
+    let time_slice = &header_buffer[14..30];
+    for mut chunk in time_slice.chunks(2) {
+        let x = chunk.read_u16::<LittleEndian>().unwrap();
+        system_time_v.push(x);
+    }
+
+    let system_time = lib::SystemTime::new(&system_time_v);
+
+    lib::FileHeader {
         file_id,
-        major_version,
-        minor_version,
+        major_version: *major_version,
+        minor_version: *minor_version,
         system_time,
-    };
-    k
+    }
 }
 
 fn main() -> io::Result<()> {
     let file = File::open("test.ess")?;
     let mut reader_buffer = BufReader::new(file);
-    
+
     let header = get_header(&mut reader_buffer);
+    dbg!(header);
 
     Ok(())
 }
